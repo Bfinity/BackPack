3e377699c73f39b589b5c7d6013da408a2fad57a
Brandy Foster <brandy.foster@detroitlabs.com>
2014-11-25 16:16:26 -0500

    added view items to fragment. set spinner

--- app/src/main/java/com/example/bfinerocks/backpack/fragments/SignUpFragment.java
+++ app/src/main/java/com/example/bfinerocks/backpack/fragments/SignUpFragment.java
@@ -5,7 +5,10 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 
 import com.example.bfinerocks.backpack.R;
 
@@ -13,14 +16,26 @@ import com.example.bfinerocks.backpack.R;
  * Created by BFineRocks on 11/21/14.
  */
 public class SignUpFragment extends Fragment {
-        EditText userName;
-        EditText userPassword;
+    EditText edtUserName;
+    EditText edtUserPassword;
+    Spinner userTypeSelector;
+    Button btnFinished;
+
 
 
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_sign_up, container, false);
+            edtUserName = (EditText) rootView.findViewById(R.id.sign_up_user_name);
+            edtUserPassword = (EditText) rootView.findViewById(R.id.sign_up_password);
+            btnFinished = (Button) rootView.findViewById(R.id.btn_done);
+            userTypeSelector = (Spinner) rootView.findViewById(R.id.spnr_user_type);
+            ArrayAdapter<CharSequence> userTypeAdapter = ArrayAdapter.createFromResource(getActivity(), R.array.spinner_user_type_values,
+                    android.R.layout.simple_spinner_item);
+            userTypeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            userTypeSelector.setAdapter(userTypeAdapter);
+
         return rootView;
         }
 }
