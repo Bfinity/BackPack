9888dbf4231cf828e258fbc7957a407e233f5a92
Brandy Foster <brandy.foster@detroitlabs.com>
2014-11-25 16:04:04 -0500

    finished basic layout for signup

--- app/src/main/java/com/example/bfinerocks/backpack/fragments/SignUpFragment.java
+++ app/src/main/java/com/example/bfinerocks/backpack/fragments/SignUpFragment.java
@@ -12,11 +12,12 @@ import com.example.bfinerocks.backpack.R;
  * Created by BFineRocks on 11/21/14.
  */
 public class SignUpFragment extends Fragment {
+    
 
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_sign_up, container, false);
         return rootView;
         }
 }
