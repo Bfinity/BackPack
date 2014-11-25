105359130693fc527a67957b2cd9b6f98484ba07
Brandy Foster <brandy.foster@detroitlabs.com>
2014-11-25 16:08:07 -0500

    added button to layout

--- app/src/main/java/com/example/bfinerocks/backpack/fragments/SignUpFragment.java
+++ app/src/main/java/com/example/bfinerocks/backpack/fragments/SignUpFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import com.example.bfinerocks.backpack.R;
 
@@ -12,7 +13,9 @@ import com.example.bfinerocks.backpack.R;
  * Created by BFineRocks on 11/21/14.
  */
 public class SignUpFragment extends Fragment {
-    
+        EditText userName;
+        EditText userPassword;
+
 
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
