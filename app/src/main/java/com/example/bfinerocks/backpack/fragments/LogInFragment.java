8328669a212846b9c9bf90eb63d7658e188b3cfa
Brandy Foster <brandy.foster@detroitlabs.com>
2014-12-03 14:12:35 -0500

    changed logic in log in fragment to include sign up link

--- app/src/main/java/com/example/bfinerocks/backpack/fragments/LogInFragment.java
+++ app/src/main/java/com/example/bfinerocks/backpack/fragments/LogInFragment.java
@@ -8,6 +8,7 @@ import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.bfinerocks.backpack.R;
@@ -17,45 +18,63 @@ import com.parse.ParseException;
 /**
  * Created by BFineRocks on 11/26/14.
  */
-public class LogInFragment extends Fragment {
+public class LogInFragment extends Fragment implements OnClickListener{
 
-    EditText userName;
-    EditText password;
-    Button btnFinished;
-
-
-
-            @Override
-            public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-                View rootView = inflater.inflate(R.layout.fragment_login, container, false);
-
-                userName = (EditText) rootView.findViewById(R.id.sign_up_user_name);
-                password = (EditText) rootView.findViewById(R.id.sign_up_password);
-                btnFinished = (Button) rootView.findViewById(R.id.btn_done);
-
-                btnFinished.setOnClickListener(new OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        ParseUserObject user = new ParseUserObject();
-                        Boolean logInSuccessful = true;
-                        try {
-                            user.signInExistingUser(userName.getText().toString(), password.getText().toString());
+    private EditText userName;
+    private EditText password;
+    private Button btnFinished;
+    private TextView signUpLink;
 
 
-                        }catch(ParseException e){
-                            Toast.makeText(getActivity(), "Log In Failed" +"\n" + e.getMessage().toUpperCase(), Toast.LENGTH_SHORT).show();
-                            logInSuccessful = false;
-                        }
 
-                        if(logInSuccessful) {
-                            getFragmentManager().beginTransaction()
-                                    .replace(R.id.container, new ClassListFragment())
-                                    .commit();
-                        }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_login, container, false);
 
-                    }
-                });
+        userName = (EditText) rootView.findViewById(R.id.sign_up_user_name);
+        password = (EditText) rootView.findViewById(R.id.sign_up_password);
+        btnFinished = (Button) rootView.findViewById(R.id.btn_done);
+        signUpLink = (TextView) rootView.findViewById(R.id.signUp_link);
 
-                return rootView;
-            }
+        btnFinished.setOnClickListener(this);
+        signUpLink.setOnClickListener(this);
+
+        return rootView;
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        switch (view.getId()){
+            case R.id.btn_done:
+                
+                ParseUserObject user = new ParseUserObject();
+                Boolean logInSuccessful = true;
+                try {
+                    user.signInExistingUser(userName.getText().toString(), password.getText().toString());
+
+
+                }catch(ParseException e){
+                    Toast.makeText(getActivity(), "Log In Failed" +"\n" + e.getMessage().toUpperCase(), Toast.LENGTH_SHORT).show();
+                    logInSuccessful = false;
+                }
+
+                if(logInSuccessful) {
+                    getFragmentManager().beginTransaction()
+                            .replace(R.id.container, new ClassListFragment())
+                            .commit();
+                }
+
+                break;
+
+            case R.id.signUp_link:
+
+                getFragmentManager().beginTransaction()
+                        .replace(R.id.container, new SignUpFragment())
+                        .addToBackStack("signUpFragment")
+                        .commit();
+
+                break;
+        }
+    }
 }
