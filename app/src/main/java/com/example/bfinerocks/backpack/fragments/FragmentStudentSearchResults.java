a5fea41f7aaf5cb4a397ebc6433934659b7e9ceb
Brandy Foster <brandy.foster@detroitlabs.com>
2014-12-05 10:10:58 -0500

    created layout for results and fragment class

---
+++ app/src/main/java/com/example/bfinerocks/backpack/fragments/FragmentStudentSearchResults.java
@@ -0,0 +1,11 @@
+package com.example.bfinerocks.backpack.fragments;
+
+import android.app.Fragment;
+
+/**
+ * Created by BFineRocks on 12/5/14.
+ */
+public class FragmentStudentSearchResults extends Fragment {
+
+    
+}
